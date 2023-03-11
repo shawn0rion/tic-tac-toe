69185af370a78bcfdce94b1f09206a151c58a7d0
shawn0rion <shawnorion07@gmail.com>
2023-03-10 18:10:34 -0600

    Play again pop up and Player objects  added.

--- app.js
+++ app.js
@@ -7,21 +7,25 @@ let boardSquares = () => {
 
         if (gameBoard.turn % 2 === 0){
             square.innerHTML = 'X';
+            square.style.color = '#ff494d';
+            gameBoard.player.board[square.id] = 'X';
         }
         if (gameBoard.turn % 2 !== 0){
             square.innerHTML = 'O';
+            square.style.color = '#2acaea';
+            gameBoard.computer.board[square.id] = 'O';
         }
 
-        gameBoard.board[square.id] = square.innerHTML;
         gameBoard.turn += 1;
 
-        gameBoard.checkBoard();
-        gameBoard.reset();
+        gameBoard.checkBoard(gameBoard.computer);
+        gameBoard.checkBoard(gameBoard.player);
     }
 
     let activateSquares = () => {
         if (gameBoard.turn === 0){
             gameBoard.squares.forEach((square) => {
+                square.style.color = '#000';
                 square.addEventListener('click', fillSquare);
             })
         }
@@ -31,40 +35,46 @@ let boardSquares = () => {
 
 let gameFlow = () => {
     
+    // introduce a check which passes each palyer to checkboard
     let positions = [[0,1,2], [3,4,5], [6,7,8],
                      [0,3,6], [1,4,7], [2,5,8],
                      [0,4,8], [2,4,6]];
-    let checkBoard = () => {
+    
+    let checkBoard = (side) => {
+        let values = side.board;
         positions.forEach((triplet) => {
-            if (gameBoard.board[triplet[0]] === gameBoard.board[triplet[1]] && 
-                gameBoard.board[triplet[0]] === gameBoard.board[triplet[2]] && 
-                gameBoard.board[triplet[0]] !== ""){
+            if (values[triplet[0]] === values[triplet[1]] && 
+                values[triplet[0]] === values[triplet[2]] && 
+                values[triplet[0]] !== ""){
                 gameBoard.gameOver = true;
+                side.declareWinner();
             }
             else if (gameBoard.turn === 9){
                 gameBoard.gameOver = true;
+                side.declareWinner();
             }
         });
     }
-    let reset = () => {
-        if (gameBoard.gameOver){
-            gameBoard.squares.forEach((square) => {
-                square.innerHTML = "";
-                gameBoard.board[square.id] = "";
-            })
-            gameBoard.gameOver = false;
-            gameBoard.turn = 0;
-            gameBoard.activateSquares();
-        }
+
+    return {checkBoard};
+}
+
+let Player = (name) => {
+    let board = new Array(9);
+    let declareWinner = () =>{
+        container.appendChild(display);
+        textbox.innerHTML = `${name} has won the game!`
+
     }
-    return {checkBoard, reset};
+    board.fill('');
+    return {name, board, declareWinner};
 }
 
 let gameBoard = (() => {
 
     let gameOver = false;
-    let board = new Array(9);
-    board.fill('');
+    let player = Player("Player");
+    let computer = Player("Computer");
     let turn = 0;
 
     let squares = document.querySelectorAll('.square');
@@ -72,7 +82,36 @@ let gameBoard = (() => {
     let {checkBoard} = gameFlow();
     let {reset} = gameFlow();
 
-    return {board, turn, squares, reset, checkBoard, activateSquares};
+    return {gameOver, player, computer, turn, squares, reset, checkBoard, activateSquares};
 })();
 
+
 gameBoard.activateSquares();
+
+// HTML for new game
+let container = document.querySelector('.container');
+let display = document.createElement('div');
+let textbox = document.createElement('div');
+let again = document.createElement('div');
+display.id = 'display';
+textbox.id = 'textbox';
+again.id = 'again';
+again.innerHTML = 'Play again?';
+again.addEventListener('click', () => {
+    if (gameBoard.gameOver){
+        gameBoard.squares.forEach((square) => {
+            square.innerHTML = "";
+            gameBoard.player = Player("Player");
+            gameBoard.computer = Player("Computer");
+        })
+        
+        gameBoard.gameOver = false;
+        gameBoard.turn = 0;
+        textbox.innerHTML = "";
+        container.removeChild(display);
+        gameBoard.activateSquares();
+    }
+})
+
+display.appendChild(textbox);
+display.appendChild(again);
